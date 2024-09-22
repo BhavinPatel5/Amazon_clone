3daee9e45e27cead855f3e9b93957fc13c0f9f63
BhavinPatel5 <95209555+BhavinPatel5@users.noreply.github.com>
2024-09-22 21:23:53 +0530

    Update Header.js

--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -4,76 +4,59 @@ import LocationOnIcon from "@mui/icons-material/LocationOn";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import SearchIcon from "@mui/icons-material/Search";
-import { allItems } from "../../Constants";
+import { useSelector, useDispatch } from "react-redux";
+import { setSearchTerm } from "../../redux/amazonSlice"; // Import setSearchTerm action
 import HeaderBottom from "./HeaderBottom";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
 
 const Header = () => {
   const [showAll, setShowAll] = useState(false);
+  const dispatch = useDispatch();
   const products = useSelector((state) => state.amazon.products);
-  console.log(products);
+
+  const handleSearchChange = (e) => {
+    const query = e.target.value;
+    dispatch(setSearchTerm(query)); // Dispatch the search term to Redux
+  };
 
   return (
-    <div className="w-full  sticky top-0 z-50">
+    <div className="w-full sticky top-0 z-50">
       <div className="w-full mx-auto bg-amazon_blue text-white px-4 py-3 flex items-center gap-4">
-        {/* logo*/}
         <Link to="/">
           <div className="headerHover">
-            <img className="w-24 mt-2" src={logo} alt="" />
+            <img className="w-24 mt-2" src={logo} alt="logo" />
           </div>
         </Link>
-        {/*Location*/}
 
         <div className="headerHover hidden mdl:inline-flex">
           <LocationOnIcon />
           <p className="text-sm text-lightText font-light flex flex-col">
-            Deliver To{" "}
-            <span className="text-sm font-semibold -mt-1 text-whiteText">
-              Chikhli
-            </span>
+            Deliver To <span className="text-sm font-semibold -mt-1 text-whiteText">Chikhli</span>
           </p>
         </div>
 
-        {/* searchbar*/}
-
-        <div className="h-10 rounded-md hidden lgl:flex flex-grow relative ">
+        <div className="h-10 rounded-md hidden lgl:flex flex-grow relative">
           <span
             onClick={() => setShowAll(!showAll)}
-            className="w-14 h-full bg-gray-200 hover:bg-gray-300 border-2 cursor-pointer duration-300 text-sm text-amazon_blue font-titleFont flex items-center justify-center rounded-tl-md rounded-bl-md">
+            className="w-14 h-full bg-gray-200 hover:bg-gray-300 border-2 cursor-pointer duration-300 text-sm text-amazon_blue font-titleFont flex items-center justify-center rounded-tl-md rounded-bl-md"
+          >
             All
             <ArrowDropDownIcon />
           </span>
-          {showAll && (
-            <div>
-              <ul className="absolute w-56 h-80 top-10 left-0 overflow-y-scroll overflow-x-hidden bg-white border-[1px] border-amazon_blue text-black p-2 flex-col gap-1 z-50">
-                {allItems.map((item) => (
-                  <li
-                    className="text-sm tracking-wide font-titleFont border-b-[1px] border-b-transparent hover:border-b-amazon_blue cursor-pointer duration-200"
-                    key={item._id}>
-                    {item.title}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
           <input
             className="h-full text-base text-amazon_blue flex-grow outline-none border-none px-2"
             type="text"
+            placeholder="Search for products..."
+            onChange={handleSearchChange} // Update search query in Redux
           />
-
           <span className="w-12 h-full flex items-center justify-center bg-amazon_yellow hover:bg-[#f3a847] duration-300 text-amazon_blue cursor-pointer rounded-tr-md rounded-br-md">
             <SearchIcon />
           </span>
         </div>
 
-        {/* sign in */}
-
         <Link to="/signin">
           <div className="flex flex-col items-start justify-center headerHover">
-            <p className="text-sm mdl:text-xs text-white mdl:text-lightText font-light">
-              Hello,Sign in
-            </p>
+            <p className="text-sm mdl:text-xs text-white mdl:text-lightText font-light">Hello, Sign in</p>
             <p className="text-sm font-semibold -mt-1 text-whiteText hidden mdl:inline-flex">
               Accounts & Lists
               <ArrowDropDownIcon />
@@ -81,14 +64,10 @@ const Header = () => {
           </div>
         </Link>
 
-        {/*Orders*/}
-
         <div className="hidden lgl:flex flex-col items-start justify-center headerHover">
-          <p className="text-xs  text-lightText font-light">Returns</p>
-          <p className="text-sm font-semibold -mt-1 text-whiteText">&Orders</p>
+          <p className="text-xs text-lightText font-light">Returns</p>
+          <p className="text-sm font-semibold -mt-1 text-whiteText">& Orders</p>
         </div>
-
-        {/*cart*/}
 
         <Link to="/Cart">
           <div className="flex items-start justify-center headerHover relative">
@@ -103,7 +82,6 @@ const Header = () => {
         </Link>
       </div>
 
-      {/*HeaderBottom*/}
       <HeaderBottom />
     </div>
   );
